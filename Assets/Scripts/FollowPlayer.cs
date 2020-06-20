e60c0b0566a4b0289b02605f9331c7eafebc0b5c
Tommy <tommylee.edu@gmail.com>
2020-06-20 00:15:33 -0400

    Bug Fix

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,11 +4,10 @@ public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
     public Vector3 cameraOffset;
-   
+
     void Update()
     {
         transform.position = player.position + cameraOffset;
-
-        
-
+    }
 }
+    
